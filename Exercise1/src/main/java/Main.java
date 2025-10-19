4481041545a56d6cfca08230174b473c3b164487
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 19:25:06 +0200

    build: print elements filterd from a list using lambda

---
+++ Exercise1/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Orange");
+        fruits.add("Watermelon");
+        fruits.add("Kiwi");
+
+        fruits.forEach((n) -> { if(n.matches(".*[oO].*")) {System.out.println(n);} });
+    }
+}
+
