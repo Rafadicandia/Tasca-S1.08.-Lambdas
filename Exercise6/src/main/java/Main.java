e50552e17ace63fd10109f9e4c863b7575cbf681
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 20:27:40 +0200

    build: sort list

--- Exercise6/src/main/java/Main.java
+++ Exercise6/src/main/java/Main.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<Object> variatedList = new ArrayList<>();
+
+        variatedList.add("Hello");
+        variatedList.add(9485685);
+        variatedList.add(94595);
+        variatedList.add("HelloWorld");
+
+        
+        variatedList.sort((s1, s2) -> Integer.compare(s2.toString().length(), s1.toString().length()));
+
+
+    }
+
 }
